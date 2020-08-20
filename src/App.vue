8136afd56ba2bf6d9328a9f3000a1caadf683769
suchanun6104101393 <suchanun0209@gmail.com>
2020-08-20 13:28:49 +0700

    393

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="https://thumbs.gfycat.com/AbandonedSimpleGlassfrog-small.gif" />
+    <HelloWorld msg="SUCHANUN WANNAPINYO" />
+
+    <hr />
+    <div class="row">
+      <div class="col-6">
+        <h2>{{ msg }}</h2>
+        <img alt="Vue logo" src="./assets/11.jpg" />
+        <h1>{{total1}}</h1>
+      </div>
+      <div class="col-6">
+        <h2>{{ msg }}</h2>
+        <img alt="Vue logo" src="./assets/22.jpg" />
+        <h1>{{total2}}</h1>
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sum" />
+      </div>
+      <div class="col-6">
+        <CsButton @cc="sum1" />
+      </div>
+      <div class="col-6">
+        <h2>{{ msg }}</h2>
+        <img alt="Vue logo" src="./assets/33.jpg" />
+        <h1>{{total3}}</h1>
+      </div>
+      <div class="col-6">
+        <h2>{{ msg }}</h2>
+        <img alt="Vue logo" src="./assets/44.jpg" />
+        <h1>{{total4}}</h1>
+      </div>
+
+      <div class="col-6">
+        <CsButton @cc="sum2" />
+      </div>
+
+      <div class="col-6">
+        <CsButton @cc="sum3" />
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import CsButton from "./components/CSButton.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CsButton,
+  },
+  data: function () {
+    return {
+      total1: 0,
+      total2: 0,
+      total3: 0,
+      total4: 0,
+    };
+  },
+  methods: {
+    sum(counterValue) {
+      this.total1 = counterValue;
+    },
+
+    sum1(counterValue) {
+      this.total2 = counterValue;
+    },
+    sum2(counterValue) {
+      this.total3 = counterValue;
+    },
+    sum3(counterValue) {
+      this.total4 = counterValue;
+    },
+  },
+};
 </script>
 
 <style>
+  
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Ultra", serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #800000;
   margin-top: 60px;
+  background: url(https://i.pinimg.com/originals/28/ae/0a/28ae0a1deeb7e1b532409e6dc6c7918d.jpg)
+    no-repeat center fixed;
+  background-size: cover;
 }
 </style>
---
+++ src/components/CSButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-warning">ถูกใจมาก {{lable}}</button>
+    <button v-on:click="downFunction" class="btn btn-info ml-1">ไม่ถูกใจอย่างแรง {{lable}}</button>
+    <button v-on:click="LoveFunction" class="btn btn-danger ml-1">รักมากมาย {{lable}}</button>
+    <button v-on:click="ResetFunction" class="btn btn-warning ml-1">รีเซ็ท {{lable}}</button>
+  </div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      counter: 0,
+    };
+  },
+  props: {
+    lable: String,
+  },
+  methods: {
+    upFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    downFunction: function () {
+      this.counter += -5;
+      this.$emit("cc", this.counter);
+    },
+
+    LoveFunction: function () {
+      this.counter += 10;
+      this.$emit("cc", this.counter);
+    },
+
+    ResetFunction: function () {
+      this.counter = 0;
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+<style>
+</style>
